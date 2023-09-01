40cfdc77994d6bfe35a30ba1656ede342e9464cc
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-02 00:04:20 +0600

    Create has-many/belongs-to relationship between Classroom and Student

--- students.rb
+++ students.rb
@@ -1,9 +1,16 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(name, age, parent_permission)
+    super(name, age, parent_permission: parent_permission)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @owner = owner
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
--- test.rb
+++ test.rb
@@ -1,6 +1,7 @@
 require_relative 'person'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
+require_relative 'students'
 
 person = Person.new(22, 'maximilianus')
 person.correct_name
@@ -8,3 +9,4 @@ capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
+p ronny = Student.new(15, 'Ronny')
