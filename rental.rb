2edcb9c9e3fb166a9ddaa10e9c6cf153431cfd8d
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-02 15:33:14 +0600

    Fix linter errors

--- rental.rb
+++ rental.rb
@@ -2,7 +2,7 @@ class Rental
   attr_accessor :date, :book, :person
 
   def initialize(book, person, date: Time.now)
-    @date = Time.now
+    @date = date
     @book = book
     book.rentals << self
 
