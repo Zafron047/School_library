683116cc06a5add0e020fe328617bdbc91c7b403
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-05 16:05:42 +0600

    Shift methods from main file to a separate Module.
    Shift display method from app to separate module

--- app.rb
+++ app.rb
@@ -152,16 +152,16 @@ class App
     puts 'Rental created successfully'
   end
 
-  def display_menu
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-  end
+  # def display_menu
+  #   puts 'Please choose an option by entering a number:'
+  #   puts '1 - List all books'
+  #   puts '2 - List all people'
+  #   puts '3 - Create a person'
+  #   puts '4 - Create a book'
+  #   puts '5 - Create a rental'
+  #   puts '6 - List all rentals for a given person id'
+  #   puts '7 - Exit'
+  # end
 
   def search_rental_by_id
     print 'ID of the person: '
--- main.rb
+++ main.rb
@@ -1,45 +1,48 @@
 require_relative 'app'
+require_relative 'welcome_message'
+require_relative 'option_input'
 
-def display_start
-  puts 'Welcome to School Library App terminal!'
-end
 
-def user_input
-  gets.chomp.to_i
-end
+# def display_start
+#   puts 'Welcome to School Library App terminal!'
+# end
 
-def option_list(app, option)
-  actions = {
-    1 => :book_list,
-    2 => :people_list,
-    3 => :create_person,
-    4 => :create_a_book,
-    5 => :create_a_rental,
-    6 => :search_rental_by_id,
-    7 => :exit_terminal
-  }
+# def user_input
+#   gets.chomp.to_i
+# end
 
-  action = actions[option]
-  if action
-    app.send(action)
-  else
-    puts 'Invalid input, please try again.'
-  end
-end
+# def option_list(app, option)
+#   actions = {
+#     1 => :book_list,
+#     2 => :people_list,
+#     3 => :create_person,
+#     4 => :create_a_book,
+#     5 => :create_a_rental,
+#     6 => :search_rental_by_id,
+#     7 => :exit_terminal
+#   }
 
-def exit_terminal
-  puts 'Exiting terminal....'
-  exit(0)
-end
+#   action = actions[option]
+#   if action
+#     app.send(action)
+#   else
+#     puts 'Invalid input, please try again.'
+#   end
+# end
+
+# def exit_terminal
+#   puts 'Exiting terminal....'
+#   exit(0)
+# end
 
 def main
   app = App.new
+
   loop do
-    display_start
-    app.display_menu
-    option = user_input
-    option_list(app, option)
-    puts
+    WelcomeMessage.display_start
+    WelcomeMessage.display_menu
+    option = OptionInput.user_input
+    OptionInput.option_list(app, option)
   end
 end
 
